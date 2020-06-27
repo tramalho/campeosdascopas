abcf4b7d2cd5c7e923e1bd1853e11f5a39959118
tramalho <thiagofacul@gmail.com>
2020-06-27 18:10:40 -0300

    88. Alimentando tabelas: UITableViewDatasource

--- CampeosDasCopas/src/Controllers/WinnersTableViewController.swift
+++ CampeosDasCopas/src/Controllers/WinnersTableViewController.swift
@@ -10,37 +10,38 @@ import UIKit
 
 class WinnersTableViewController: UITableViewController {
 
+    private var worldcups: [WorldCup] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        loadWorldCups()
     }
 
+    func loadWorldCups() {
+        do {
+            if let fileUrl = Bundle.main.url(forResource: "winners", withExtension: "json") {
+                let jsonData = try Data(contentsOf: fileUrl)
+                worldcups = try JSONDecoder().decode([WorldCup].self, from: jsonData)
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return worldcups.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
 
         // Configure the cell...
 
         return cell
     }
-    */
+ 
 
     /*
     // Override to support conditional editing of the table view.
